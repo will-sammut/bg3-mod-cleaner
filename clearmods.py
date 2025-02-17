251a14a0b688d1fc3024a978d51c0ceefaee7b32
Will Sammut <willsammut10@gmail.com>
2025-02-17 17:38:28 +0000

    removed gui, fixed logic

--- clearmods.py
+++ clearmods.py
@@ -1,35 +1,24 @@
 import os 
-import tkinter as tk
 import platform
-from tkinter import messagebox
 
-root = tk.Tk()
-root.withdraw()
+print(f"OS: {platform.system()}")
 
-# Show the message box
+File = input("Paste the path to the mod list: ")
+with open(File) as modlist:
+    Result1 = print("BG3 Mod Cleaner \n\nPlease paste your Baldur's Gate 3 installation path\n  e.g. \n../Steam/steamapps/common/Baldurs Gate 3/")
+    installPath = input()
 
-Result0 = messagebox.showinfo("BG3 Mod Cleaner", "Please open the modlist file.")
-if Result0 == 'ok':
-    File = tk.filedialog.askopenfile()
-    with open(File.name) as modlist:
-        with open("clearmods_log.txt", "w") as log:
-            Result1 = messagebox.showinfo("BG3 Mod Cleaner", "Please select your Baldur's Gate 3 installation path\n  e.g. \n../Steam/steamapps/common/Baldurs Gate 3/")
-            if Result1 == 'ok':
-                installPath = tk.filedialog.askdirectory()
+    print(f"Removing leftover files from {installPath}")
 
-                for mod in modlist:
-                    if platform.system() == 'Windows':
-                        ModPath = installPath.rstrip() + mod.rstrip().replace("\\", "/")
-                    else:
-                        ModPath = installPath.rstrip() + mod.rstrip()
+    for mod in modlist:
+        ModPath = installPath.rstrip() + mod.rstrip()
 
-                    try:
-                        os.remove(ModPath)
-                        log.write(f"removing {ModPath}....OK\n")
-                    except FileNotFoundError:
-                        log.write(f"removing {ModPath}....NOT FOUND\n")
+        print(ModPath)
+        try:
+            os.remove(ModPath)
+            print(f"removing {ModPath}....OK\n")
+        except FileNotFoundError:
+            print(f"removing {ModPath}....NOT FOUND\n")
 
-                messagebox.showinfo("BG3 Mod Cleaner", "Done.")
+        print("\n\n....DONE")
 
-# Run the Tkinter event loop
-root.mainloop()
