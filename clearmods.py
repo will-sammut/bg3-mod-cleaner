080fe1efd9e8e10335a7b9f70351769778d93213
Will Sammut <59370067+will-sammut@users.noreply.github.com>
2025-02-17 16:25:37 +0000

    Added logs

--- clearmods.py
+++ clearmods.py
@@ -8,29 +8,29 @@ root = tk.Tk()
 root.withdraw()
 
 # Show the message box
-Result0 = messagebox.showinfo("BG3 Mod Cleaner", "Please open the modlist file.")
 
+Result0 = messagebox.showinfo("BG3 Mod Cleaner", "Please open the modlist file.")
 if Result0 == 'ok':
     File = tk.filedialog.askopenfile()
     with open(File.name) as modlist:
+        with open("clearmods_log.txt", "w") as log:
+            Result1 = messagebox.showinfo("BG3 Mod Cleaner", "Please select your Baldur's Gate 3 installation path\n  e.g. \nSteam/steamapps/common/Baldurs Gate 3/")
+            if Result1 == 'ok':
+                installPath = tk.filedialog.askdirectory()
 
-        Result1 = messagebox.showinfo("BG3 Mod Cleaner", "Please select your Baldur's Gate 3 installation path\n  e.g. \nSteam/steamapps/common/Baldurs Gate 3/")
-        if Result1 == 'ok':
-            installPath = tk.filedialog.askdirectory()
+                for mod in modlist:
+                    if platform.system() == 'Windows':
+                        ModPath = installPath.rstrip() + mod.rstrip().replace("\\", "/")
+                    else:
+                        ModPath = installPath.rstrip() + mod.rstrip()
 
-            for mod in modlist:
-                if platform.system() == 'Windows':
-                    ModPath = installPath.rstrip() + mod.rstrip().replace("\\", "/")
-                else:
-                    ModPath = installPath.rstrip() + mod.rstrip()
+                    try:
+                        os.remove(ModPath)
+                        log.write(f"removing {ModPath}....OK\n")
+                    except FileNotFoundError:
+                        log.write(f"removing {ModPath}....NOT FOUND\n")
 
-                try:
-                    os.remove(ModPath)
-                    print(f"removing {ModPath}....OK")
-                except FileNotFoundError:
-                    print(f"removing {ModPath}....NOT FOUND")
-
-            messagebox.showinfo("BG3 Mod Cleaner", "Done.")
+                messagebox.showinfo("BG3 Mod Cleaner", "Done.")
 
 # Run the Tkinter event loop
 root.mainloop()
